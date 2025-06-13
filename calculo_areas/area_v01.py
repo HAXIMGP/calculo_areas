7dd916c568634cfc8af2f1fea1cdb1389aae2aae
Holguer Alejandro Narvaez Solano <153919186+HAXIMGP@users.noreply.github.com>
2025-06-12 21:09:01 -0500

    Calculo de area rectangulo

--- calculo_areas/area_v01.py
+++ calculo_areas/area_v01.py
@@ -22,6 +22,17 @@ def calcular_area_triangulo():
         messagebox.showerror("Error", "Por favor ingresa un valor numérico válido.")
         return 0
 
+def calcular_area_rectangulo():
+    try:
+        ancho = float(entry_ancho.get())
+        alto = float(entry_alto.get())
+        area_rectangulo = ancho * alto
+        messagebox.showinfo("Resultado", f"El área del rectángulo es: {area_rectangulo}")
+        return area_rectangulo
+    except ValueError:
+        messagebox.showerror("Error", "Por favor ingresa un valor numérico válido.")
+        return 0
+
 def mostrar_cuadrado():
     limpiar_ventana()
     
@@ -95,6 +106,45 @@ def mostrar_triangulo():
     )
     btn_volver_seleccion.pack(pady=10)
 
+def mostrar_rectangulo():
+    limpiar_ventana()
+
+    tk.Label(root, text="Ancho (Rectángulo):", font=("Helvetica", 12), bg="#40E0D0").pack(pady=5)
+    global entry_ancho
+    entry_ancho = tk.Entry(root, font=("Helvetica", 12))
+    entry_ancho.pack(pady=5)
+
+    tk.Label(root, text="Alto (Rectángulo):", font=("Helvetica", 12), bg="#40E0D0").pack(pady=5)
+    global entry_alto
+    entry_alto = tk.Entry(root, font=("Helvetica", 12))
+    entry_alto.pack(pady=5)
+
+    btn_calcular_rectangulo = tk.Button(
+        root,
+        text="Calcular Área del Rectángulo",
+        command=calcular_area_rectangulo,
+        font=("Helvetica", 12, "bold"),
+        bg="#FF6347",
+        fg="white",
+        relief="raised",
+        bd=4,
+        padx=10, pady=5
+    )
+    btn_calcular_rectangulo.pack(pady=10)
+
+    btn_volver_seleccion = tk.Button(
+        root,
+        text="Volver a seleccionar",
+        command=mostrar_opciones,
+        font=("Helvetica", 12, "bold"),
+        bg="#FF6347",
+        fg="white",
+        relief="raised",
+        bd=4,
+        padx=10, pady=5
+    )
+    btn_volver_seleccion.pack(pady=10)
+
 def limpiar_ventana():
     for widget in root.winfo_children():
         widget.destroy()
@@ -127,6 +177,19 @@ def mostrar_opciones():
         padx=10, pady=5
     )
     btn_triangulo.pack(pady=10)
+
+    btn_rectangulo = tk.Button(
+        root,
+        text="Área del Rectángulo",
+        command=mostrar_rectangulo,
+        font=("Helvetica", 12, "bold"),
+        bg="#FF6347",
+        fg="white",
+        relief="raised",
+        bd=4,
+        padx=10, pady=5
+    )
+    btn_rectangulo.pack(pady=10)
 
 root = tk.Tk()
 root.title("Calculadora de Áreas")
